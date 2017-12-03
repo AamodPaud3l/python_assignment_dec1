d53121b5da1a56379441e62011224918406d6441
AamodPaud3l <paudelaamod@gmail.com>
2017-12-03 21:14:16 +0545

    Program to calculate the average expenditure

---
+++ simplescript.py
@@ -0,0 +1,15 @@
+#Program to find the average expenditure
+
+food = input("How much was spent?")
+travel = input("How much was spent?")
+accessories = input("How much was spent?")
+clothing = input("How much was spent?")
+education = input("How much was spent?")
+health = input("How much was spent?")
+
+total = food + travel + accessories + clothing + education + health
+average = total / 6
+
+print average
+
+
