b0415c0ad60922dca3d55dfbd3f1528d104688e7
AamodPaud3l <paudelaamod@gmail.com>
2017-12-03 22:07:06 +0545

    Program to write Birthday song

---
+++ birthday.py
@@ -0,0 +1,7 @@
+#Program to wish Birthday to friend
+
+name = input('Enter the name of your friend: ')
+def birthday(name):
+    print('Happy  birthday  to  you\nHappy  birthday  to  you\nHappy  birthday  dear {0}\nHappy  birthday  to  you!\n'.format(name))
+
+birthday(name)
